720e9df41b31a48c2265b2af9ceebc18e2e00dff
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2023-06-18 19:06:38 -0300

    feat: add schemas

---
+++ schema/__init__.py
@@ -0,0 +1,2 @@
+from schema.user_schema import CreateUserRequest, UserOutputResponse, UserListResponse, user_to_output
+from schema.error_schema import ErrorResponse
---
+++ schema/task_schema.py
@@ -0,0 +1,42 @@
+from typing import List, Optional
+from pydantic import BaseModel
+
+from model.task import Task
+
+
+class CreateTaskRequest(BaseModel):
+    """Definição do objeto da requisição de criação 
+    de uma nova tarefa"""
+
+    title: str = "Criação da tela de login"
+    story: str = "Eu como usuário quero poder logar no sistem..."
+    due_date: str = "2018-11-15T00:00:00"
+    is_done: Optional[bool] = False
+
+
+class TaskOutputResponse(BaseModel):
+    """Definição da resposta de criação ou busca de uma
+    tarefa"""
+
+    title: str = "Criação da tela de login"
+    story: str = "Eu como usuário quero poder logar no sistem..."
+    due_date: str = "2018-11-15T00:00:00"
+    is_done: Optional[bool] = False
+
+
+class TaskListResponse(BaseModel):
+    """Definição da resposta de listagem de
+    usuários"""
+
+    tasks: List[Task]
+
+
+def task_to_output(task: Task) -> dict:
+    """Mapeia o mdelo de tarefa para a visualização do cliente
+    """
+    return {
+        "title": task.title,
+        "story": task.story,
+        "due_date": task.due_date,
+        "is_done": task.is_done
+    }
